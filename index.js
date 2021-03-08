51bba2f66240a690dab6f47559923b371acf1f18
Awourrobberrt <sophierobberrt@gmail.com>
2021-03-08 20:45:57 +0300

    first attempt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function fruits(fruit_list){
     fruit_list=["strawberry","kiwi","guava","mangoes","tomatoes"];
-    console.log(fruit_list);
+    fruit_list.forEach(element=>console.log(element))
+    console.log(fruit_list[0]);
 }
 fruits();
